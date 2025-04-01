047e172b61889696b14139397da1084042437595
David Ellis <david@conquerthelawn.com>
2025-03-31 23:32:01 -0500

    revising install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,20 @@
 #!/usr/bin/env bash
 
+# We will be using the `meta` key as mac-os 'Cmd' key, so we
+# must prevent it from doing the regular gnome overlay action:
+gsettings set org.gnome.mutter overlay-key ''
+
 # install keyd
 git clone https://github.com/rvaiya/keyd
 cd keyd
 make
 sudo make install
+sudo systemctl enable --now keyd
+
 cd ..
 rm -rf ./keyd
 
+sudo mkdir -p /etc/keyd/
 sudo cp default.conf /etc/keyd/default.conf
 sudo chmod 644 /etc/keyd/default.conf
 
